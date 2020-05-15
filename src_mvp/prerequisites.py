de3112c3b8dfdaca0be785877fb65113cd5307b9
melevir <melevir@gmail.com>
2020-05-16 00:35:48 +0300

    goodbye, mvp

--- src_mvp/prerequisites.py
+++
@@ -1,9 +0,0 @@
-from typing import Mapping, Any
-
-
-def python_only(repo_config: Mapping[str, Any]) -> bool:
-    return 'python' in repo_config.get('main_languages', '')
-
-
-def rus_only(repo_config: Mapping[str, Any]) -> bool:
-    return repo_config.get('language') == 'ru'
