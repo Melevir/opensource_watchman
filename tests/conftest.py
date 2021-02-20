a369cfed515192892001c90bc8072fcc71f1e481
Ilya Lebedev <melevir@gmail.com>
2021-02-20 23:36:37 +0300

    add tests on codeclimate api

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,18 @@
 import pytest
 import responses
 
+from opensource_watchman.api.codeclimate_api import CodeClimateAPI
 from opensource_watchman.common_types import RepoResult
 
 
 @pytest.fixture
-def ok_repo_result():
+def ok_repo_result(owner, repo_name):
     return RepoResult(
-        owner='owner',
+        owner=owner,
         package_name='test',
         description='test description',
         badges_urls=[],
-        repo_name='test_repo',
+        repo_name=repo_name,
         errors={},
     )
 
@@ -20,3 +21,28 @@ def ok_repo_result():
 def mocked_responses():
     with responses.RequestsMock() as rsps:
         yield rsps
+
+
+@pytest.fixture
+def owner():
+    return 'owner'
+
+
+@pytest.fixture
+def repo_name():
+    return 'test_repo'
+
+
+@pytest.fixture
+def api_token():
+    return '123-secret-api-token'
+
+
+@pytest.fixture
+def code_climate_api(mocked_responses, owner, repo_name, api_token):
+    mocked_responses.add(
+        responses.GET,
+        f'https://api.codeclimate.com/v1/repos?github_slug={owner}/{repo_name}',
+        json={'data': [{'id': 123}]},
+    )
+    return CodeClimateAPI.create(owner, repo_name, api_token)
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,7 @@
 import deal
 import responses
 
+from opensource_watchman.api.codeclimate_api import CodeClimateAPI
 from opensource_watchman.api.pypistats import get_pypi_downloads_stat
 from opensource_watchman.api.travis import TravisRepoAPI
 from opensource_watchman.pipelines.extended_repo_info import fetch_downloads_stat
@@ -13,7 +14,7 @@ def test_get_pypi_stats(mocked_responses):
     mocked_responses.add(
         responses.GET,
         f'https://pypistats.org/api/packages/{test_project_name}/recent',
-        json={"data": {"last_week": 1}},
+        json={'data': {'last_week': 1}},
     )
 
     assert get_pypi_downloads_stat(test_project_name)['last_week'] == 1
@@ -23,6 +24,24 @@ def test_fetch_downloads_stat(ok_repo_result, mocked_responses):
     mocked_responses.add(
         responses.GET,
         'https://pypistats.org/api/packages/test/recent',
-        json={"data": {"last_week": 1}},
+        json={'data': {'last_week': 1}},
     )
     assert fetch_downloads_stat([ok_repo_result]) == {'test_repo': 1}
+
+
+def test_codeclimate_get_repo_id(mocked_responses, owner, repo_name, api_token):
+    mocked_responses.add(
+        responses.GET,
+        f'https://api.codeclimate.com/v1/repos?github_slug={owner}/{repo_name}',
+        json={'data': [{'id': 123}]},
+    )
+    assert CodeClimateAPI(owner, repo_name, api_token, None).get_repo_id() == 123
+
+
+def test_codeclomate_test_coverage(mocked_responses, code_climate_api):
+    mocked_responses.add(
+        responses.GET,
+        'https://api.codeclimate.com/v1/repos/123/test_reports',
+        json={'data': [{'attributes': {'covered_percent': 99}}]},
+    )
+    assert code_climate_api.get_test_coverage() == 99
