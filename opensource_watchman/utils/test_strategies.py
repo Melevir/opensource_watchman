8a173502b492cc42c94c1ca9f8794ee9417406aa
Ilya Lebedev <melevir@gmail.com>
2021-02-23 11:05:09 +0300

    using default strategies for types

--- opensource_watchman/utils/test_strategies.py
+++ opensource_watchman/utils/test_strategies.py
@@ -1,4 +1,7 @@
-from hypothesis.strategies import from_type, composite, one_of, text, sampled_from
+from inspect import signature
+
+from deal import cases as deal_cases, cached_property
+from hypothesis.strategies import from_type, composite, one_of, text, sampled_from, SearchStrategy
 from typing import Callable
 
 from opensource_watchman.common_types import GithubPipelineData
@@ -10,6 +13,7 @@ def github_data_with_yaml_config(draw: Callable) -> GithubPipelineData:
         sampled_from([
             'foo:\n  - bar  - baz',
             'foo: 1\nbar: 2',
+            'foo\nbar: 2',  # invalid yaml
             '',
         ]),
         text(),
@@ -21,3 +25,19 @@ def github_data_with_yaml_config(draw: Callable) -> GithubPipelineData:
         {'main_languages': 'python'},
     ]))
     return item
+
+
+class cases(deal_cases):
+    types_to_strategy_map = {
+        GithubPipelineData: github_data_with_yaml_config()
+    }
+
+    @cached_property
+    def strategy(self) -> SearchStrategy:
+        kwargs = self.kwargs.copy()
+        for parameter_name, parameter in signature(self.func).parameters.items():
+            param_type = parameter.annotation
+            if param_type in self.types_to_strategy_map:
+                kwargs[parameter_name] = self.types_to_strategy_map[param_type]
+        self.kwargs = kwargs
+        return super().strategy
--- tests/test_pipelines.py
+++ tests/test_pipelines.py
@@ -13,38 +13,35 @@ from opensource_watchman.pipelines.master import (
     compose_pull_requests_updated_at, has_no_stale_pull_requests, create_master_pipeline,
 )
 from opensource_watchman.pipelines.travis import create_travis_pipeline
-from opensource_watchman.utils.test_strategies import github_data_with_yaml_config
+from opensource_watchman.utils.test_strategies import cases
 
 
-test_github_create_api = deal.cases(create_api)
-test_github_fetch_project_description = deal.cases(fetch_project_description)
-test_create_github_pipeline = deal.cases(create_github_pipeline)
-test_create_travis_pipeline = deal.cases(create_travis_pipeline)
-test_master_has_readme = deal.cases(has_readme)
-test_master_has_required_sections_in_readme = deal.cases(has_required_sections_in_readme)
-test_master_has_ci_config = deal.cases(has_ci_config)
-test_master_is_ci_bild_status_ok = deal.cases(is_ci_bild_status_ok)
-test_master_has_ci_badge_in_readme = deal.cases(has_ci_badge_in_readme)
-test_master_has_ci_weekly_build_enabled = deal.cases(has_ci_weekly_build_enabled)
-test_master_fetch_package_name = deal.cases(fetch_package_name)
-test_master_has_package_name = deal.cases(has_package_name)
-test_master_is_package_on_pypi = deal.cases(is_package_on_pypi)
-test_master_has_commits_in_last_n_months = deal.cases(has_commits_in_last_n_months)
-test_master_fetch_test_coverage_badge_url = deal.cases(fetch_test_coverage_badge_url)
-test_master_is_project_exists_on_codeclimate = deal.cases(is_project_exists_on_codeclimate)
-test_master_has_test_coverage_info = deal.cases(has_test_coverage_info)
-test_master_is_test_coverage_fine = deal.cases(is_test_coverage_fine)
-test_master_is_test_coverage_badge_exists = deal.cases(is_test_coverage_badge_exists)
-test_master_fetch_issues_stale_days = deal.cases(fetch_issues_stale_days)
-test_master_has_enough_actual_issues = deal.cases(has_enough_actual_issues)
-test_master_analyze_is_prs_ok_to_merge = deal.cases(analyze_is_prs_ok_to_merge)
-test_master_compose_pull_requests_updated_at = deal.cases(compose_pull_requests_updated_at)
-test_master_has_no_stale_pull_requests = deal.cases(has_no_stale_pull_requests)
-test_master_create_master_pipeline = deal.cases(create_master_pipeline)
-test_master_has_support_of_python_versions = deal.cases(
-    has_support_of_python_versions,
-    kwargs={'github_data': github_data_with_yaml_config()},
-)
+test_github_create_api = cases(create_api)
+test_github_fetch_project_description = cases(fetch_project_description)
+test_create_github_pipeline = cases(create_github_pipeline)
+test_create_travis_pipeline = cases(create_travis_pipeline)
+test_master_has_readme = cases(has_readme)
+test_master_has_required_sections_in_readme = cases(has_required_sections_in_readme)
+test_master_has_ci_config = cases(has_ci_config)
+test_master_is_ci_bild_status_ok = cases(is_ci_bild_status_ok)
+test_master_has_ci_badge_in_readme = cases(has_ci_badge_in_readme)
+test_master_has_ci_weekly_build_enabled = cases(has_ci_weekly_build_enabled)
+test_master_fetch_package_name = cases(fetch_package_name)
+test_master_has_package_name = cases(has_package_name)
+test_master_is_package_on_pypi = cases(is_package_on_pypi)
+test_master_has_commits_in_last_n_months = cases(has_commits_in_last_n_months)
+test_master_fetch_test_coverage_badge_url = cases(fetch_test_coverage_badge_url)
+test_master_is_project_exists_on_codeclimate = cases(is_project_exists_on_codeclimate)
+test_master_has_test_coverage_info = cases(has_test_coverage_info)
+test_master_is_test_coverage_fine = cases(is_test_coverage_fine)
+test_master_is_test_coverage_badge_exists = cases(is_test_coverage_badge_exists)
+test_master_fetch_issues_stale_days = cases(fetch_issues_stale_days)
+test_master_has_enough_actual_issues = cases(has_enough_actual_issues)
+test_master_analyze_is_prs_ok_to_merge = cases(analyze_is_prs_ok_to_merge)
+test_master_compose_pull_requests_updated_at = cases(compose_pull_requests_updated_at)
+test_master_has_no_stale_pull_requests = cases(has_no_stale_pull_requests)
+test_master_create_master_pipeline = cases(create_master_pipeline)
+test_master_has_support_of_python_versions = cases(has_support_of_python_versions)
 
 
 def test_fetch_pull_request_details(github_api, detailed_pull_requests, mocked_responses):
